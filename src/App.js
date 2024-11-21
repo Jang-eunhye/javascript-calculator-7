18b06194e07a8dd4087d7ce63cdfac3b719d5395
Jang Eunhye <hh961130@naver.com>
2024-11-21 20:49:09 +0900

    feat: 입력문자열 유효성 검사 구조 설계

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import inputView from "./inputView.js";
+import validate from "./validate.js";
 class App {
   async run() {
     const inputString = await inputView.getInputString();
+    inputString = validate.inputValidate(inputString);
   }
 }
 
---
+++ src/validate.js
@@ -0,0 +1,10 @@
+import { Console } from "@woowacourse/mission-utils";
+
+class validate {
+    inputValidate(inputString) {
+        return inputString;
+    }
+}
+
+export default validate;
+
