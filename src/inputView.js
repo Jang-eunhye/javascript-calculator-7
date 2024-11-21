33db9111abf126ed24be9275a4020fe7435b6f15
Jang Eunhye <hh961130@naver.com>
2024-11-21 20:41:26 +0900

    fix: inputView.js 수정

--- src/inputView.js
+++ src/inputView.js
@@ -3,6 +3,7 @@ import { Console } from "@woowacourse/mission-utils";
 const inputView = {
     async getInputString(){
         let inputString = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+        return inputString;
     }
 }
 export default inputView;
